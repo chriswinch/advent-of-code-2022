ad0b3b018ec165bad70074e2cbfdd929bea4e680
Chris Winch <hello@chriswinch.me>
2022-12-02 10:34:55 +0000

    refactor day 1 parts into one file

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-  let contents = fs::read_to_string("../input.txt").expect("can't find file");
+  let contents = fs::read_to_string("input.txt").expect("can't find file");
 
   let split: Vec<Vec<u64>> = contents.split("\n\n")
     .map(|par| par.lines().map(|l| l.parse().unwrap()).collect())
@@ -13,6 +13,18 @@ fn main() {
     summed_vals.push(sum);
   }
 
+  // Part 1
+  let max = summed_vals.iter().max().unwrap();
+  println!("Part One: max {}", max);
+
+  // Part 2
   summed_vals.sort();
-  println!("total: {}", summed_vals.iter().rev().take(3).sum::<u64>());
+  println!(
+    "Part Two - total: {}",
+    summed_vals
+      .iter()
+      .rev()
+      .take(3)
+      .sum::<u64>()
+  );
 }
